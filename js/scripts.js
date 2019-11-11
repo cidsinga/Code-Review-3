3ad684ec5c3daabbcf805b34dea3e105c7ab597a
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-11-10 16:47:24 -0800

    cleaned up bracketing and display issues.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 function beepBooper(numberInput) {
   var arr = [];
 
-  for (var i = 0; i <= numberInput; i++)
+  for (var i = 0; i <= numberInput; i++) {
     if (i.toString().includes("3")) {
       arr.push("I'm sorry, Dave. I'm afraid I can't do that.");
     } else if (i.toString().includes("2")) {
@@ -10,15 +10,18 @@ function beepBooper(numberInput) {
     } else if (i.toString().includes("1")) {
       arr.push("beep");
       console.log(arr);
+    } else {
+      arr.push(i.toString());
     }
-    return arr;
-
-    }
-    beepBooper()
+  }
+  return arr;
+}
 
 $(document).ready(function(){
   $('#inputForm').submit(function(event){
-  event.preventDefault();
-  var numberInput = parseInt($('input#numberInput').val());
+    event.preventDefault();
+    var numberInput = parseInt($('input#numberInput').val());
+    var output = beepBooper(numberInput)
+    console.log(output);
   });
 });
